043786be0cf6836ef4b2297fd6a9f5a16992b712
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-11-19 20:11:11 +0300

    WaitGroup

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -10,17 +11,31 @@ import (
 // Вывести в консоль время выполнения каждого запроса
 func main() {
 	t := time.Now()
+
+	var wg sync.WaitGroup // представляет собой по сути счетчик, который прибавляется на 1 при вызове wg.Add(1) и уменьшается на 1 при вызове wg.Done()
+	
 	for i := 0; i < 10; i++ {
-		go getHttp()
+		wg.Add(1) // говорим WaitGroup-е, что закинули 1 горутину
+		// go getHttp(&wg) // обязательно передаем по указателю, тк хотим работать с WaitGroup-ой wg, а иначе в функции создастся другая waitgroup-а (копия)
+		
+		// 2 способ использования WaitGroup-ы, без передачи ее в функцию (функция не должна знать о WaitGroup)  
+		go func() {
+			getHttp()
+			wg.Done()
+		}()
 	}
-	time.Sleep(time.Second*3)
-	fmt.Printf("Total time: %v", time.Since(t) - time.Second*3 ) // +- 4sec без goroutine, меньее полсекунды с goroutine
+	// В результате после цикла счетчик WaitGroup-ы будет равен 10, каждая выполненная горутина когда будет выполнена уменьшает счетчик (планировщик планирует ее выполнение сам) на 1
+	wg.Wait() // ждем завершения всех горутин
+	fmt.Printf("Total time: %v", time.Since(t))
 	
 }
 
 
+// func getHttp(wg *sync.WaitGroup){
 func getHttp(){
+	// defer wg.Done() // откладываем на после выхода из функции уменьшение счетчика WaitGroup-ы
 	t := time.Now()
+
 	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Println(err)
