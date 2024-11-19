8c917d9934ab2ab33cc51e01b2ac8a51e286935f
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-11-19 19:49:59 +0300

    Упражнение: параллельные запросы и сравнение с последовательным выполнением

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
+// 10 конкурентных запросов на GET по адресу google.com
+// Вывести в консоль время выполнения каждого запроса
 func main() {
-	go fmt.Println("Hello from goroutine") // заставляем выполняться в отдельной горутине (планируется планировщиком выполнение)
-	fmt.Println("Hello from main")
-	go fmt.Println("Hello from goroutine 2") // будет разный порядок выполнеия горутин
-	time.Sleep(time.Nanosecond) // ожидаем завершения горутины (успеет все выполниться за наносекунду)
+	t := time.Now()
+	for i := 0; i < 10; i++ {
+		go getHttp()
+	}
+	time.Sleep(time.Second*3)
+	fmt.Printf("Total time: %v", time.Since(t) - time.Second*3 ) // +- 4sec без goroutine, меньее полсекунды с goroutine
+	
+}
+
+
+func getHttp(){
+	t := time.Now()
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(time.Since(t)) // смотрим сколько времени прошло
+	fmt.Printf("StatusCode: %v\n", resp.StatusCode)
 }
